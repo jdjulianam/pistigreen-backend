ac1365cbd7c41cfb3086aa3014d126a4c788b53d
juliana <juliana.medina.peaku.io>
2024-06-03 16:29:01 -0500

    Configurando el archivo wsgi.py

--- backend_pistigreen/wsgi.py
+++ backend_pistigreen/wsgi.py
@@ -13,6 +13,6 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_pistigreen.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pistigreen-backend.settings')
 application = get_wsgi_application()
 app = application
