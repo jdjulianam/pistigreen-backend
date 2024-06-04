fb7579604a89c04b392b24b531d8708a5d7b46d2
juliana <juliana.medina.peaku.io>
2024-06-04 08:42:21 -0500

    Cambios en bd

--- backend_pistigreen/settings.py
+++ backend_pistigreen/settings.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 from pathlib import Path
 import environ
 import os
+import dj_database_url
 
 # Inicializar la instancia de environ
 env_parser = environ.Env()
@@ -37,10 +38,11 @@ if EMAIL_BACKEND == "django.core.mail.backends.smtp.EmailBackend":
 
 
 # Configuración de la base de datos
+DATABASE_URL = os.getenv("DATABASE_URL")
 DATABASES = {
-    'default': env_parser.db()
+    
+    'default': dj_database_url.parse(DATABASE_URL)
 }
-
 # Application definition
 
 INSTALLED_APPS = [
